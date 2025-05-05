d545da585b9d4f10d8a81f4c950ed18a4ddfd82a
ngocthao <21130542@st.hcmuaf.edu.vn>
2025-05-05 22:01:15 +0700

    fix logo

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { AppBar, Toolbar, Typography, Button, TextField, Box, IconButton } from '@mui/material';
-import { Search, AccountCircle } from '@mui/icons-material';
+import { Search, AccountCircle, LogoDev } from '@mui/icons-material';
 import { Link } from 'react-router-dom'; // 👉 Thêm dòng này
+import logo from '../assets/images/logo.png';
 
 export default function Header() {
   const [searchQuery, setSearchQuery] = useState('');
@@ -19,8 +20,8 @@ export default function Header() {
       <Toolbar>
         {/* Logo */}
         <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/4/4a/CGV_Logo.svg" alt="CGV Logo" style={{ height: '40px', marginRight: '10px' }} />
-          CGV
+          <img src={logo} alt="CGV Logo" style={{ height: '50px', marginRight: '10px' }} />
+          Cinema
         </Typography>
 
         {/* Menu */}
