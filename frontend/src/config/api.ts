77ae0f9b8a759014b808b6f4696c4f08d064fc66
ngocthao <21130542@st.hcmuaf.edu.vn>
2025-05-30 11:53:09 +0700

    them config

---
+++ frontend/src/config/api.ts
@@ -0,0 +1,11 @@
+const BASE_URL = "http://localhost:8080"; 
+const BOOKING_URL=`${BASE_URL}/booking`
+
+const API_URLS = {
+  BOOKING: {
+    chooseSeat: `${BOOKING_URL}/choose-seat`,
+  },
+};
+
+
+export default API_URLS;
